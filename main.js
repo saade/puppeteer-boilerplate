e579cb559be227dd0166e19ebce5ad748f8f9c2a
SaadeMotion <saade@outlook.com.br>
2020-05-05 15:24:18 -0300

    Added chromium and puppeteer-core

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 const { Client } = require("./index");
 const { ReactDevTools } = require("./extensions");
+const chromium = require("chromium");
 
 (async () => {
+  const client = new Client({
+    puppeteer: {
+      headless: false,
+      executablePath: chromium.path,
+      args: [
+        `--disable-extensions-except=${ReactDevTools}`,
+        `--load-extension=${ReactDevTools}`,
+      ],
+    },
+  });
+
   console.log(`Eat Sleep Code Repeat!`);
 })();
